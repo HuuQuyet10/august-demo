26afccaa91aaa9182766e372f01aa3a68b97a381
Quyet Pham <quyet.ph@isofhcare.com>
2021-01-18 10:02:42 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import Content from "./components/Content"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
